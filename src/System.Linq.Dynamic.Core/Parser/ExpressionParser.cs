28364a750dd0b65596acf4bb3fdb862a7b89f910
Fryderyk Huang <fryderyk.huang@gmail.com>
2019-04-24 18:17:30 +0800

    Revert "Revert "Add type casting support.""
    This reverts commit c912e574f5e6eefe897fe7aee991d131e5b3fce1.

--- src/System.Linq.Dynamic.Core/Parser/ExpressionParser.cs
+++ src/System.Linq.Dynamic.Core/Parser/ExpressionParser.cs
@@ -943,6 +943,17 @@ namespace System.Linq.Dynamic.Core.Parser
             _textParser.NextToken();
             Expression e = ParseConditionalOperator();
             _textParser.ValidateToken(TokenId.CloseParen, Res.CloseParenOrOperatorExpected);
+
+            var constExp = e as ConstantExpression;
+            if (constExp != null && constExp.Value is Type)
+            {
+                _textParser.NextToken();
+                var nextExpression = ParseConditionalOperator();
+
+                // cast: (constExp)nextExpression
+                return Expression.Convert(nextExpression, (Type)constExp.Value);
+            }
+
             _textParser.NextToken();
             return e;
         }
@@ -1465,6 +1476,12 @@ namespace System.Linq.Dynamic.Core.Parser
 
                 type = typeof(Nullable<>).MakeGenericType(type);
                 _textParser.NextToken();
+            }
+
+
+            if (_textParser.CurrentToken.Id == TokenId.CloseParen)
+            {
+                return Expression.Constant(type);
             }
 
             // This is a shorthand for explicitely converting a string to something
